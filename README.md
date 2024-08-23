be4b2cc689f497df49c70fc9a0eb5ac823da0c6e
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2024-08-23 18:50:47 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# project-name
-A description
+Orion PDF
+Java PDF library 
--- src/main/java/io/github/orionlibs/orion_pdf/OrionDomain.java
+++ src/main/java/io/github/orionlibs/orion_pdf/OrionDomain.java
@@ -1,4 +1,4 @@
-package io.github.orionlibs.project_name;
+package io.github.orionlibs.orion_pdf;
 
 public class OrionDomain
 {
